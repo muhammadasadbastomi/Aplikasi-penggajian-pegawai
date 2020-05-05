5082856aec671c17bf33e54a46e19e0811a2e4fb
muhammadasadbastomi <tomycules@gmail.com>
2020-05-05 09:32:03 +0800

    add pegawai relation

--- app/User.php
+++ app/User.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -16,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'role', 'photos'
+        'name', 'email', 'password', 'role', 'photos',
     ];
 
     /**
@@ -36,4 +35,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function pegawai()
+    {
+        return $this->HasOne(Pegawai::class);
+    }
 }
