73a43a3edd77fbd2b5fed69b0579aa7b07ffd596
dedovis <dedovis@gmail.com>
2020-05-05 23:20:26 +0800

    fix route

--- resources/views/layouts/admin/includes/sidebar.blade.php
+++ resources/views/layouts/admin/includes/sidebar.blade.php
@@ -1,12 +1,14 @@
 <aside class="main-sidebar elevation-4 sidebar-dark-danger">
     <!-- Brand Logo -->
     <a href="{{ route('adminIndex') }}" class="brand-link text-sm navbar-secondary">
-        <img src="{{asset('admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{asset('admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
     <!-- Sidebar -->
-    <div class="sidebar os-host os-theme-light os-host-overflow os-host-overflow-y os-host-resize-disabled os-host-scrollbar-horizontal-hidden os-host-transition">
+    <div
+        class="sidebar os-host os-theme-light os-host-overflow os-host-overflow-y os-host-resize-disabled os-host-scrollbar-horizontal-hidden os-host-transition">
         <div class="os-resize-observer-host observed">
             <div class="os-resize-observer" style="left: 0px; right: auto;"></div>
         </div>
@@ -15,12 +17,14 @@
         </div>
         <div class="os-content-glue" style="margin: 0px -8px; width: 249px; height: 519px;"></div>
         <div class="os-padding">
-            <div class="os-viewport os-viewport-native-scrollbars-invisible os-viewport-native-scrollbars-overlaid" style="overflow-y: scroll;">
+            <div class="os-viewport os-viewport-native-scrollbars-invisible os-viewport-native-scrollbars-overlaid"
+                style="overflow-y: scroll;">
                 <div class="os-content" style="padding: 0px 8px; height: 100%; width: 100%;">
                     <!-- Sidebar user panel (optional) -->
                     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                         <div class="image">
-                            <img src="{{asset('admin/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                            <img src="{{asset('admin/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2"
+                                alt="User Image">
                         </div>
                         <div class="info">
                             <a href="#" class="d-block">{{ Auth::user()->name }}</a>
@@ -28,7 +32,8 @@
                     </div>
                     <!-- Sidebar Menu Data -->
                     <nav class="mt-2">
-                        <ul class="nav nav-pills nav-sidebar flex-column text-sm nav-legacy nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
+                        <ul class="nav nav-pills nav-sidebar flex-column text-sm nav-legacy nav-child-indent"
+                            data-widget="treeview" role="menu" data-accordion="false">
                             <!-- Sidebar Menu Data Master Admin -->
                             @if(auth()->user()->role == 'admin')
                             <li class="nav-item has-treeview menu-open">
@@ -40,6 +45,18 @@
                                     </p>
                                 </a>
                                 <ul class="nav nav-treeview" style="display: block;">
+                                    <li class="nav-item">
+                                        <a href="{{route('jabatanIndex')}}" class="nav-link active">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>Data Jabatan</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{route('golonganIndex')}}" class="nav-link active">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>Golongan</p>
+                                        </a>
+                                    </li>
                                     <li class="nav-item">
                                         <a href="{{route('pegawaiIndex')}}" class="nav-link active">
                                             <i class="far fa-circle nav-icon"></i>
@@ -58,113 +75,36 @@
                                             <p>Data Gaji Pegawai</p>
                                         </a>
                                     </li>
-                                    <li class="nav-item">
-                                        <a href="{{route('jabatanIndex')}}" class="nav-link active">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Data Jabatan</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="{{route('golonganIndex')}}" class="nav-link active">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Golongan</p>
-                                        </a>
-                                    </li>
                                 </ul>
                             </li>
-                            @endif
+
                             <!-- End Sidebar Menu Data Master Admin -->
                             <li class="nav-item">
-                                <a href="{{ route('userIndex') }}" class="nav-link">
+                                <a href="{{route('gajiIndex')}}" class="nav-link active">
+                                    <i class="nav-icon fas fa-money-check-alt"></i>
+                                    <p>
+                                        Data Gaji Pegawai
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('userIndex') }}" class="nav-link active">
                                     <i class="nav-icon fas fa-user"></i>
                                     <p>
                                         Admin
                                     </p>
                                 </a>
                             </li>
-                            <li class="nav-item has-treeview">
-                                <a href="#" class="nav-link">
-                                    <i class="nav-icon fas fa-copy"></i>
+                            <li class="nav-item">
+                                <a href="{{route('absensiIndex')}}" class="nav-link active">
+                                    <i class="nav-icon fas fa-file-alt"></i>
                                     <p>
-                                        Layout Options
-                                        <i class="fas fa-angle-left right"></i>
-                                        <span class="badge badge-info right">6</span>
+                                        Data Absensi Pegawai
                                     </p>
                                 </a>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item">
-                                        <a href="pages/layout/top-nav.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Top Navigation</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Top Navigation + Sidebar</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="pages/layout/boxed.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Boxed</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="pages/layout/fixed-sidebar.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Fixed Sidebar</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="pages/layout/fixed-topnav.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Fixed Navbar</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="pages/layout/fixed-footer.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Fixed Footer</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="pages/layout/collapsed-sidebar.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Collapsed Sidebar</p>
-                                        </a>
-                                    </li>
-                                </ul>
                             </li>
-                            <li class="nav-item has-treeview">
-                                <a href="#" class="nav-link">
-                                    <i class="nav-icon fas fa-chart-pie"></i>
-                                    <p>
-                                        Charts
-                                        <i class="right fas fa-angle-left"></i>
-                                    </p>
-                                </a>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item">
-                                        <a href="pages/charts/chartjs.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>ChartJS</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="pages/charts/flot.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Flot</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="pages/charts/inline.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Inline</p>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
+                            @endif
+
                         </ul>
 
                     </nav>
