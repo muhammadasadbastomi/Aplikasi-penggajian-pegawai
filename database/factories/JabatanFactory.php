a832f63079ceb4df74ac77be2542247eae9861f4
muhammadasadbastomi <tomycules@gmail.com>
2020-05-05 09:33:09 +0800

    delete kode jabatan faker

--- database/factories/JabatanFactory.php
+++ database/factories/JabatanFactory.php
@@ -3,16 +3,16 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Jabatan;
-use Faker\Generator as Faker;
 use Faker\Factory as FakerID;
+use Faker\Generator as Faker;
 
 $factory->define(Jabatan::class, function (Faker $faker) {
     $faker = FakerID::create('id_ID');
     return [
         'uuid' => $faker->uuid,
-        'kode_jabatan' => $faker->unique()->word(20),
         'jabatan' => $faker->word(20),
-        'keterangan' => $faker->word(30),
+        'gaji_pokok' => '5000000',
+        'tunjangan' => '5000000',
     ];
-    
+
 });
