997ce629149f7bc21e90a82c292176dc240a34de
muhammadasadbastomi <tomycules@gmail.com>
2020-05-05 09:33:01 +0800

    delete kode_golongan faker

--- database/factories/GolonganFactory.php
+++ database/factories/GolonganFactory.php
@@ -3,14 +3,13 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Golongan;
-use Faker\Generator as Faker;
 use Faker\Factory as FakerID;
+use Faker\Generator as Faker;
 
 $factory->define(Golongan::class, function (Faker $faker) {
     $faker = FakerID::create('id_ID');
     return [
         'uuid' => $faker->uuid,
-        'kode_golongan' => $faker->unique()->word(20),
         'golongan' => $faker->word(20),
         'keterangan' => $faker->word(30),
     ];
