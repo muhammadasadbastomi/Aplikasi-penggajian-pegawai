c727fa79019bd17e27a5899fbbb52710732b25c5
dedovis <dedovis@gmail.com>
2020-05-06 05:10:21 +0800

    fix report

--- resources/views/laporan/cetak_jabatan.blade.php
+++ resources/views/laporan/cetak_jabatan.blade.php
@@ -1,43 +1,143 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Export PDF Dinas Perhubungan Kota Banjarbaru</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+        .logo {
+            float: left;
+            margin-right: 0px;
+            width: 15%;
+            padding: 0px;
+            text-align: right;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid;
+            padding-left: 5px;
+            text-align: center;
+        }
+
+        .judul {
+            text-align: center;
+        }
+
+        .headtext {
+            float: right;
+            margin-left: 0px;
+            width: 75%;
+            padding-left: 0px;
+            padding-right: 10%;
+        }
+
+        .ttd {
+            margin-left: 70%;
+            text-align: center;
+            text-transform: uppercase;
+        }
+
+        .sizeimg {
+            width: 60px;
+        }
+
+        .headtext {
+            float: right;
+            margin-left: 0px;
+            width: 75%;
+            padding-left: 0px;
+            padding-right: 10%;
+        }
+
+        .header {
+            margin-bottom: 0px;
+            text-align: center;
+            height: 150px;
+            padding: 0px;
+        }
+
+        .ttd {
+            margin-left: 70%;
+            text-align: center;
+            text-transform: uppercase;
+        }
+
+        hr {
+            margin-top: 10%;
+            height: 3px;
+            background-color: black;
+        }
+
+        br {
+            margin-bottom: 5px !important;
+        }
+
+        h5 {
+            line-height: 0.3;
+        }
+    </style>
 </head>
 
 <body>
-    <style type="text/css">
-        table tr td,
-        table tr th {
-            font-size: 9pt;
-        }
-    </style>
-    <h5 class="sorting text-center">Laporan Data Jabatan Pegawai Dinas Perhubungan Kota Banjarbaru</h5>
-    <h6 class="sorting text-center"><a style="margin-top: 12px; color: black;">Laporan Data Jabatan Pegawai</a>
-    </h6>
-    <br>
-    <table class='table table-bordered'>
-        <thead>
-            <tr>
-                <th class="sorting text-center">No</th>
-                <th class="sorting_asc text-center" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Jabatan</th>
-                <th class="sorting text-center" tabindex="1" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Gaji Pokok</th>
-                <th class="sorting text-center" tabindex="2" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Tunjangan</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($jabatan as $d)
-            <tr>
-                <td class="text-center">{{$loop->iteration}}</td>
-                <td class="text-center">{{$d->jabatan}}</td>
-                <td class="text-center">{{$d->gaji_pokok}}</td>
-                <td class="text-center">{{$d->tunjangan}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
 
+    <div class="header">
+        <div class="logo">
+            <img class="sizeimg" src="img/bjb.png">
+        </div>
+        <div class="headtext">
+            <h3 style="margin:0px;">PEMERINTAH KOTA BANJARBARU</h3>
+            <h1 style="margin:0px;">DINAS PERHUBUNGAN</h1>
+            <p style="margin:0px;">Alamat : Jl. Jenderal Sudirman No.3 Telp. ( 0511 ) 6749034 Banjarbaru</p>
+        </div>
+        <hr>
+    </div>
+
+    <div class="container">
+        <h2 style="text-align:center;text-transform: uppercase;">laporan data jabatan</h2>
+        <table class='table table-bordered'>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Jabatan
+                    </th>
+                    <th>Gaji Pokok</th>
+                    <th>Tunjangan</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($jabatan as $d)
+                <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$d->jabatan}}</td>
+                    <td>{{$d->gaji_pokok}}</td>
+                    <td>{{$d->tunjangan}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <br>
+        <br>
+        <div class="ttd">
+            <h5>
+                Banjarbaru,
+            </h5>
+            <h5>isi jabatan</h5>
+            <br>
+            <br>
+            <h5 style="text-decoration:underline;">nama pejabat</h5>
+            <h5>golongan / kode golongan</h5>
+            <h5>NIP.</h5>
+        </div>
+    </div>
 </body>
 
 </html>
