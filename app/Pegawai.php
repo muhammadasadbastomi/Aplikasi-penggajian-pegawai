2443244452952a21f13a9731add73a12cdf5c53a
muhammadasadbastomi <tomycules@gmail.com>
2020-05-05 09:31:55 +0800

    add user and golongan relation model

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -2,8 +2,8 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
 use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
 class Pegawai extends Model
@@ -11,11 +11,21 @@ class Pegawai extends Model
     use Notifiable;
     use Uuid;
     protected $fillable = [
-        'nama', 'nik', 'tempat_lahir', 'tgl_lahir', 'tgl_masuk', 'user_id', 'photos', 'role', 'jabatan_id'
+        'nama', 'nik', 'tempat_lahir', 'tgl_lahir', 'tgl_masuk', 'user_id', 'photos', 'role', 'jabatan_id', 'golongan_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function jabatan()
     {
         return $this->belongsTo(Jabatan::class);
+    }
+
+    public function golongan()
+    {
+        return $this->belongsTo(Golongan::class);
     }
 }
