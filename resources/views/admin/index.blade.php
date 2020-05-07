026f3db7c6cad42ce8555d0cb5e990643ef86938
muhammadasadbastomi <tomycules@gmail.com>
2020-05-07 10:06:35 +0800

    add usecase absensi

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,13 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Dashboard</h1>
+                <h1 class="m-0 text-dark">
+                    @if($cek === 2)
+                    Dashboard
+                    @else
+                    Dashboard Absensi Periode Bulan {{carbon\carbon::now()->translatedFormat('F Y')}}
+                    @endif
+                </h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
@@ -84,6 +90,68 @@
                 </div>
             </div>
             <!-- ./col -->
+            @if(Auth::user()->role =='pegawai')
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-3 col-6">
+                        <div class="info-box mb-3 bg-success">
+                            <span class="info-box-icon"><i class="fas fa-thumbs-up"></i></span>
+
+                            <div class="info-box-content">
+                                <a id="hadir" disabled="disabled" href="{{route('absensiUserHadir')}}">
+                                    <span class="info-box-text text-white">Hadir</span>
+                                    <span class="info-box-number text-white">*****</span>
+                                </a>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="info-box mb-3 bg-info">
+                            <span class="info-box-icon"><i class="far fa-comment"></i></span>
+
+                            <div class="info-box-content">
+                                <a href="{{route('absensiUserHadir')}}">
+                                    <span class="info-box-text text-white">Izin</span>
+                                    <span class="info-box-number text-white">****</span>
+                                </a>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="info-box mb-3 bg-warning">
+                            <span class="info-box-icon"><i class="fas fa-tag"></i></span>
+
+                            <div class=" info-box-content">
+                                <a href="{{route('absensiUserHadir')}}">
+                                    <span class="info-box-text text-black">Tanpa Keterangan </span>
+                                    <span class="info-box-number text-black">*****************</span>
+                                </a>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="info-box mb-3 bg-danger">
+                            <span class="info-box-icon"><i class="far fa-heart"></i></span>
+
+                            <div class="info-box-content">
+                                <a href="{{route('absensiUserHadir')}}">
+                                    <span class="info-box-text text-white">Sakit</span>
+                                    <span class="info-box-number text-white">*****</span>
+                                </a>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                    </div>
+                    <div class="row">
+                        <p>Note : {{$keterangan}}</p>
+                    </div>
+                </div>
+            </div>
+            @else
+            @endif
         </div>
         <!-- /.row -->
     </div><!-- /.container-fluid -->
