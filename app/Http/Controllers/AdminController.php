1f3a0725318c1877a4f2e2c453f24c15a382f1c2
muhammadasadbastomi <tomycules@gmail.com>
2020-05-07 10:04:46 +0800

    create index function beetwen user and pegawai

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,14 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Absensi;
+use App\Periode;
+use Auth;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
-    // admin dashboard 
+    // admin dashboard
     public function index()
     {
-        return view('admin.index');
+        $cek_periode = Periode::orderBy('created_at', 'desc')->first();
+        $month = carbon::parse($cek_periode->periode)->format('m');
+        $now = carbon::now()->format('m');
+
+        if (Auth::user()->role == 'pegawai') {
+            if ($month == $now) {
+                $cek = 1;
+            } else {
+                $cek = 0;
+            }
+        } else {
+            $cek = 2;
+        }
+        if (Auth::user()->role == 'pegawai') {
+            $id = Auth::user()->pegawai->id;
+
+            $absensi = Absensi::where('periode_id', $cek_periode->id)->where('pegawai_id', $id)->first();
+            // dd($absensi);
+
+            if (isset($absensi)) {
+                $cek_absensi = carbon::parse($absensi->created_at)->format('d');
+                $day = carbon::now()->format('d');
+
+                if ($cek_absensi == $day) {
+                    $keterangan = 'Anda sudah melakukan absensi';
+                } else {
+                    $keterangan = 'Anda belum melakukan absensi';
+                }
+            } else {
+                $keterangan = 'Anda belum melakukan absensi';
+            }
+
+            return view('admin.index', compact('cek', 'keterangan'));
+        } else {
+
+            return view('admin.index', compact('cek'));
+        }
+
     }
 
     // bpk data view
