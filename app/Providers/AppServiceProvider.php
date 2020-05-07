68b3f2e0c3f441bd7b1f753d55138d56291fddd9
muhammadasadbastomi <tomycules@gmail.com>
2020-05-07 10:05:23 +0800

    setup locale ID for carbon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // For example im gonna locale all dates to Indonesian (ID)
+        config(['app.locale' => 'id']);
+        \Carbon\Carbon::setLocale('id');
+
     }
 
     /**
