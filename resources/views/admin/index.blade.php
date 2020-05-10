46bc73cd444ea9f9623557230d9e7477e1958f88
aryudi33 <33aryudi@gmail.com>
2020-05-10 22:00:01 +0800

    Renew Dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -32,152 +32,95 @@
         <div class="row">
             <div class="col-lg-3 col-6">
                 <!-- small box -->
-                <div class="small-box bg-info">
-                    <div class="inner">
-                        <h3>150</h3>
-
-                        <p>Total Karyawan</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
-                    </div>
-
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
                 <!-- small box -->
-                <div class="small-box bg-success">
-                    <div class="inner">
-                        <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                        <p>Karyawan Kontrak</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
-                    </div>
-
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-warning">
-                    <div class="inner">
-                        <h3>44</h3>
-
-                        <p>Karyawan Tetap</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-person-add"></i>
-                    </div>
-
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-danger">
-                    <div class="inner">
-                        <h3>65</h3>
-
-                        <p>Unique Visitors</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-pie-graph"></i>
-                    </div>
-
-                </div>
-            </div>
-            <!-- ./col -->
-            @if(Auth::user()->role =='pegawai' && $absensi != null)
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-3 col-6">
-                        <div class="info-box mb-3 bg-success">
-                            <span class="info-box-icon"><i class="fas fa-thumbs-up"></i></span>
-
-                            <div class="info-box-content">
-                                <a id="hadir" disabled="disabled" href="{{route('absensiUserHadir')}}">
-                                    <span class="info-box-text text-white">Hadir</span>
-                                    <span class="info-box-number text-white">*****</span>
-                                    @if($absensi->hadir == 1 && $absensi->status == 1)
-                                    <h3>✔<sup style="font-size: 20px"></sup></h3>
-                                    @else
-                                    <h3 class="text-body">-</h3>
-                                    @endif
-                                </a>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="info-box mb-3 bg-info">
-                            <span class="info-box-icon"><i class="far fa-comment"></i></span>
-
-                            <div class="info-box-content">
-                                <a href="{{route('absensiUserIzin')}}">
-                                    <span class="info-box-text text-white">Izin</span>
-                                    <span class="info-box-number text-white">****</span>
-                                    @if($absensi->izin == 1 && $absensi->status == 1)
-                                    <h3>✔<sup style="font-size: 20px"></sup></h3>
-                                    @else
-                                    <h3 class="text-body">-</h3>
-                                    @endif
-                                </a>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="info-box mb-3 bg-warning">
-                            <span class="info-box-icon"><i class="fas fa-tag"></i></span>
-
-                            <div class=" info-box-content">
-                                <a>
-                                    <span class="info-box-text text-black">Tanpa Keterangan </span>
-                                    <span class="info-box-number text-black">*****************</span>
-                                    @if($absensi->alfa == 1 && $absensi->status == 1)
-                                    <h3>✔<sup style="font-size: 20px"></sup></h3>
-                                    @else
-                                    <h3 class="text-body">-</h3>
-                                    @endif
-                                </a>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="info-box mb-3 bg-danger">
-                            <span class="info-box-icon"><i class="far fa-heart"></i></span>
-
-                            <div class="info-box-content">
-                                <a href="{{route('absensiUserSakit')}}">
-                                    <span class="info-box-text text-white">Sakit</span>
-                                    <span class="info-box-number text-white">*****</span>
-                                    @if($absensi->sakit == 1 && $absensi->status == 1)
-                                    <h3>✔<sup style="font-size: 20px"></sup></h3>
-                                    @else
-                                    <h3 class="text-body">-</h3>
-                                    @endif
-                                </a>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                    </div>
+                @if(Auth::user()->role =='pegawai' && $absensi != null)
+                <div class="card-body">
                     <div class="row">
-                        <p>Note : {{$keterangan}}</p>
+                        <div class="col-lg-3 col-6">
+                            <div class="info-box mb-3 bg-success">
+                                <span class="info-box-icon"><i class="fas fa-thumbs-up"></i></span>
+
+                                <div class="info-box-content">
+                                    <a id="hadir" disabled="disabled" href="{{route('absensiUserHadir')}}">
+                                        <span class="info-box-text text-white">Hadir</span>
+                                        <span class="info-box-number text-white">*****</span>
+                                        @if($absensi->hadir == 1 && $absensi->status == 1)
+                                        <h3>✔<sup style="font-size: 20px"></sup></h3>
+                                        @else
+                                        <h3 class="text-body">-</h3>
+                                        @endif
+                                    </a>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="info-box mb-3 bg-info">
+                                <span class="info-box-icon"><i class="far fa-comment"></i></span>
+
+                                <div class="info-box-content">
+                                    <a href="{{route('absensiUserIzin')}}">
+                                        <span class="info-box-text text-white">Izin</span>
+                                        <span class="info-box-number text-white">****</span>
+                                        @if($absensi->izin == 1 && $absensi->status == 1)
+                                        <h3>✔<sup style="font-size: 20px"></sup></h3>
+                                        @else
+                                        <h3 class="text-body">-</h3>
+                                        @endif
+                                    </a>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="info-box mb-3 bg-warning">
+                                <span class="info-box-icon"><i class="fas fa-tag"></i></span>
+
+                                <div class=" info-box-content">
+                                    <a>
+                                        <span class="info-box-text text-black">Tanpa Keterangan </span>
+                                        <span class="info-box-number text-black">*****************</span>
+                                        @if($absensi->alfa == 1 && $absensi->status == 1)
+                                        <h3>✔<sup style="font-size: 20px"></sup></h3>
+                                        @else
+                                        <h3 class="text-body">-</h3>
+                                        @endif
+                                    </a>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="info-box mb-3 bg-danger">
+                                <span class="info-box-icon"><i class="far fa-heart"></i></span>
+
+                                <div class="info-box-content">
+                                    <a href="{{route('absensiUserSakit')}}">
+                                        <span class="info-box-text text-white">Sakit</span>
+                                        <span class="info-box-number text-white">*****</span>
+                                        @if($absensi->sakit == 1 && $absensi->status == 1)
+                                        <h3>✔<sup style="font-size: 20px"></sup></h3>
+                                        @else
+                                        <h3 class="text-body">-</h3>
+                                        @endif
+                                    </a>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                        </div>
+                        <div class="row">
+                            <p>Note : {{$keterangan}}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            @elseif(Auth::user()->role == 'admin')
-            @else
-            <h2 class="text-danger">Periode belum dibuat</h2>
+                @elseif(Auth::user()->role == 'admin')
+                @else
+                <h2 class="text-danger">Periode belum dibuat</h2>
 
-            @endif
-        </div>
-        <!-- /.row -->
-    </div><!-- /.container-fluid -->
+                @endif
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
 </section>
 <!-- /.content -->
 @endsection
