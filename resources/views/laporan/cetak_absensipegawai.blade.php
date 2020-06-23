e9ccea5175e9051c88b2e8373eecc4fa3fcfc92a
muhammadasadbastomi <tomycules@gmail.com>
2020-06-23 15:36:18 +0800

    update

--- resources/views/laporan/cetak_absensipegawai.blade.php
+++ resources/views/laporan/cetak_absensipegawai.blade.php
@@ -100,19 +100,30 @@
         <hr>
     </div>
     <div class="container">
-        <h2 style="text-align:center; margin-top:-25px;">Laporan Data Absensi Pegawai Kontrak {{$pegawai->nama}} Periode {{\carbon\carbon::parse($start)->translatedFormat('d F Y')}} - {{\carbon\carbon::parse($end)->translatedFormat('d F Y')}}</h2>
-        <table id="example1" class="table table-bordered table-striped dataTable dtr-inline collapsed" role="grid" aria-describedby="example1_info">
+        <h2 style="text-align:center; margin-top:-25px;">Laporan Data Absensi Pegawai Kontrak {{$pegawai->nama}} Periode
+            {{\carbon\carbon::parse($start)->translatedFormat('d F Y')}} -
+            {{\carbon\carbon::parse($end)->translatedFormat('d F Y')}}</h2>
+        <table id="example1" class="table table-bordered table-striped dataTable dtr-inline collapsed" role="grid"
+            aria-describedby="example1_info">
             <thead>
                 <tr role="row">
                     <th>No</th>
-                    <th class="sorting text-center" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Tanggal</th>
-                    <th class="sorting text-center" tabindex="1" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Nama</th>
-                    <th class="sorting text-center" tabindex="2" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Izin</th>
-                    <th class="sorting text-center" tabindex="3" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Sakit</th>
-                    <th class="sorting text-center" tabindex="4" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Alfa</th>
-                    <th class="sorting text-center" tabindex="5" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Hadir</th>
-                    <th class="sorting text-center" tabindex="6" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Keterangan</th>
-                    <th class="sorting text-center" tabindex="7" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Status</th>
+                    <th class="sorting text-center" tabindex="0" aria-controls="example1" rowspan="1" colspan="1"
+                        aria-label="Browser: activate to sort column ascending">Tanggal</th>
+                    <th class="sorting text-center" tabindex="1" aria-controls="example1" rowspan="1" colspan="1"
+                        aria-label="Browser: activate to sort column ascending">Nama</th>
+                    <th class="sorting text-center" tabindex="2" aria-controls="example1" rowspan="1" colspan="1"
+                        aria-label="Platform(s): activate to sort column ascending">Izin</th>
+                    <th class="sorting text-center" tabindex="3" aria-controls="example1" rowspan="1" colspan="1"
+                        aria-label="Platform(s): activate to sort column ascending">Sakit</th>
+                    <th class="sorting text-center" tabindex="4" aria-controls="example1" rowspan="1" colspan="1"
+                        aria-label="Platform(s): activate to sort column ascending">Alfa</th>
+                    <th class="sorting text-center" tabindex="5" aria-controls="example1" rowspan="1" colspan="1"
+                        aria-label="Platform(s): activate to sort column ascending">Hadir</th>
+                    <th class="sorting text-center" tabindex="6" aria-controls="example1" rowspan="1" colspan="1"
+                        aria-label="Platform(s): activate to sort column ascending">Keterangan</th>
+                    <th class="sorting text-center" tabindex="7" aria-controls="example1" rowspan="1" colspan="1"
+                        aria-label="Platform(s): activate to sort column ascending">Status</th>
                     {{-- <th></th> --}}
             </thead>
             <tbody>
@@ -128,29 +139,29 @@
                         <td class="text-center">{{$d->pegawai->nama}}</td>
                         <td class="text-center">
                             @if($d->izin == 1)
-                            <b style="color:red">v</b>
-                            @else
-                            -
-                            @endif</td>
+                        <td style="font-family: DejaVu Sans, sans-serif;">✔</td>
+                        @else
+                        -
+                        @endif</td>
                         <td class="text-center">
                             @if($d->sakit == 1)
-                            <b style="color:red">v</b>
-                            @else
-                            -
-                            @endif
+                        <td style="font-family: DejaVu Sans, sans-serif;">✔</td>
+                        @else
+                        -
+                        @endif
                         </td>
                         <td class="text-center">
                             @if($d->alfa == 1)
-                            <b style="color:red">v</b>
-                            @else
-                            -
-                            @endif</td>
+                        <td style="font-family: DejaVu Sans, sans-serif;">✔</td>
+                        @else
+                        -
+                        @endif</td>
                         <td class="text-center">
                             @if($d->hadir == 1)
-                            <b style="color:red">v</b>
-                            @else
-                            -
-                            @endif
+                        <td style="font-family: DejaVu Sans, sans-serif;">✔</td>
+                        @else
+                        -
+                        @endif
                         </td>
                         <td class="text-center">
                             @if(isset($d->keterangan))
@@ -164,7 +175,8 @@
                             @if($d->hadir == 1 && $d->status == 3
                             || $d->izin == 1 && $d->status == 3
                             || $d->sakit == 1 && $d->status == 3)
-                            <a class="btn btn-xs btn-primary text-white" href="{{route('absensiVerifikasi', ['id' => $d->uuid])}}"><i></i> Verifikasi</a>
+                            <a class="btn btn-xs btn-primary text-white"
+                                href="{{route('absensiVerifikasi', ['id' => $d->uuid])}}"><i></i> Verifikasi</a>
                             @elseif($d->status == 1)
                             <a class="btn btn-xs btn-success text-white"><i class="fas fa-check"></i>
                                 Terverifikasi</a>
